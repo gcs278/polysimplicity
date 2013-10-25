61f68bb950565497ff9c2864263b0dbdf199a0f5
David Villacis <development@davicompu.com>
2013-10-25 15:07:09 -0400

    uploading examples
    Examples of database

---
+++ alt/docs/Model_example.php
@@ -0,0 +1,14 @@
+<?php
+
+class Model_Example extends ORM {
+
+    protected $_table_name = 'example';
+    
+    /*
+    Relationships, joins done by ORM
+    protected $_has_many = array (
+			'internationals'=> array(
+                        'model' => 'Assignment',
+                        'foreign_key' => 'usfq_id'));
+    */  
+}
